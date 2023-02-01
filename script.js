06d26a21830d1c524d180a9492f74fcdad3bf075
Wiktor Miernik <102859974+Miernio@users.noreply.github.com>
2023-02-01 20:36:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+let TaskList=new Array();
+let TaskStatus=new Array();
+const ToDoBtn=document.querySelector('#todo');
+
+window.addEventListener("load",DataFromLocalStorage);
+ToDoBtn.addEventListener("click",ToDoApp);
+
+function DataFromLocalStorage(){
+    if(localStorage.getItem('TaskListInMiernioToDoApp')!=""){
+        TaskList=localStorage.getItem('TaskListInMiernioToDoApp').split(',');
+        TaskStatus=localStorage.getItem('TaskStatusInMiernioToDoApp').split(',');
+    }else{
+        //pass
+ }
+    DisplayTask();
+}
+
+function AddNewTask(){
+    let Task=document.getElementById('task').value;
+    TaskList.push(Task);
+    TaskStatus.push('0');
+    console.log("Pomyślno dodano zadanie: "+TaskList[TaskList.length-1]+" Ze statusem: "+TaskStatus[TaskStatus.length-1]);
+    //console.log(TaskList.length);
+}
+
+function DisplayTask(){
+    document.getElementById('nieroz').innerHTML="";
+    document.getElementById('roz').innerHTML="";
+    document.getElementById('zak').innerHTML="";
+    for(let i=0;i<TaskList.length;i++){
+        if(TaskStatus[i]=='0'){
+            document.getElementById('nieroz').innerHTML+="<li>"+TaskList[i]+"<button type='button' onclick='DelateTask("+i+")'><i class='fa-solid fa-x'></i></button><button type='button' onclick='ToStarted("+i+")'><i class='fa-solid fa-arrow-right'></button></li>";
+        }
+        else if(TaskStatus[i]=='1'){
+            document.getElementById('roz').innerHTML+="<li>"+TaskList[i]+"<button type='button' onclick='DelateTask("+i+")'><i class='fa-solid fa-x'></i></button><button type='button' onclick='ToFinished("+i+")'><i class='fa-solid fa-arrow-right'></i></button><button type='button' onclick='ToNotStarted("+i+")'><i class='fa-solid fa-arrow-left'></i></button></li>";
+        }
+        else if(TaskStatus[i]=='2'){
+            document.getElementById('zak').innerHTML+='<li>'+TaskList[i]+"<button type='button' onclick='DelateTask("+i+")'><i class='fa-solid fa-x'></i></button><button type='button' onclick='ToStarted("+i+")'><i class='fa-solid fa-arrow-left'></i></button></li>";
+        }else{
+            //pass
+        }
+    }
+}
+ToStarted=(licz)=>{
+    TaskStatus[licz]='1';
+    DisplayTask();
+    SaveLocalStorage();
+}
+ToNotStarted=(licz)=>{
+    TaskStatus[licz]='0';
+    DisplayTask();
+    SaveLocalStorage();
+}
+ToFinished=(licz)=>{
+    TaskStatus[licz]='2';
+    DisplayTask();
+    SaveLocalStorage();
+}
+DelateTask=(licz)=>{
+    TaskStatus[licz]='3';
+    DisplayTask();
+    SaveLocalStorage();
+}
+function ToDoApp(){
+    AddNewTask();
+    DisplayTask();
+    SaveLocalStorage();
+}
+function SaveLocalStorage(){
+    localStorage.setItem('TaskListInMiernioToDoApp',TaskList);
+    localStorage.setItem('TaskStatusInMiernioToDoApp',TaskStatus);
+}
